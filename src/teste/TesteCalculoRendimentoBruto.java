3edc3a591568cd7be9aaae7ab5206f10da1246c4
Bruno Matias Casas <leguenta@hotmail.com>
2018-09-21 17:48:09 -0300

    Triangulando teste de calculo de rendimento bruto e add mais um teste

--- src/teste/TesteCalculoRendimentoBruto.java
+++ src/teste/TesteCalculoRendimentoBruto.java
@@ -2,28 +2,41 @@ package teste;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
 import app.Calculadora;
 
+@RunWith(Parameterized.class)
 public class TesteCalculoRendimentoBruto {
-
-	@Test
-	public void test() {
-		int n = 60;
-		float p = 1000f;
-		float i = 8.5f;
-		
-		assertEquals(13.972603f, Calculadora.calculoRendimentoBruto(n,p,i),0f);
+	
+	@Parameters
+	public static Collection<Object[]> data(){
+		return Arrays.asList( new Object[][] {
+			{60,1000,8.5f,13.972603f},{120,500,8.0f,13.150684f},{240,3000,9.0f,177.53424f}
+		});
 	}
 	
+	@Parameter(0)
+	public int n;
+	@Parameter(1)
+	public float p;
+	@Parameter(2)
+	public float i;
+	@Parameter(3)
+	public float r;
+	
+	
 	@Test
-	public void test2() {
-		int n = 120;
-		float p = 500f;
-		float i = 8.0f;
-		
-		assertEquals(13.150684f, Calculadora.calculoRendimentoBruto(n,p,i),0f);
+	public void test() {
+
+		assertEquals(r, Calculadora.calculoRendimentoBruto(n,p,i),0f);
 	}
 
 }
