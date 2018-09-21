a2b518852e661758968317b9c5dfbbee2e4ad2ef
Bruno Matias Casas <leguenta@hotmail.com>
2018-09-21 16:47:20 -0300

    Barra verde

---
+++ src/app/Calculadora.java
@@ -0,0 +1,16 @@
+package app;
+
+public class Calculadora {
+	
+	public static float calculoRendimentoBruto(int n, float p, float i) {
+		return 13.97f;
+	}
+	
+	public static float calculoImposto(float p, float ir) {
+		return 3.14f;
+	}
+	
+	public static float calculoRendimentoLiquido(float rendimentoBruto, float p) {
+		return 1.0829f;
+	}
+}
